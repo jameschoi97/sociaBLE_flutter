85000fce53785dfb464559d9e9e706f0e8df393c
jameschoi97 <jameschoi97@nyu.edu>
2021-04-30 20:56:16 +0900

    no message

---
+++ lib/config/util/extensions/sociable_mediaquery.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_utils/get_utils.dart';
+
+extension SociableMediaQueryData on MediaQueryData {
+
+  void getSize(){
+
+  }
+}
--- lib/ui/pages/chatroom/chatroom_page.dart
+++ lib/ui/pages/chatroom/chatroom_page.dart
@@ -3,13 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sociable/config/constants/ui/theme_constants.dart';
 
+import 'action/chat_input_controller.dart';
+import 'action/chat_scroll_controller.dart';
+
 class ChatroomPage extends StatelessWidget {
   static final String name = '/chat_room';
 
   final _themeController = Get.find<SociableThemeController>();
+  final _chatInputController = Get.find<ChatInputController>();
+  final _chatScrollController = Get.find<ChatScrollController>();
+
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: 'Hey there', messageType: 'receiver'),
+    ChatMessage(messageContent: 'Hey you', messageType: 'sender'),
+    ChatMessage(
+        messageContent:
+            'What would happen if I make a really long string??? I really wonder what\'s gonna happen',
+        messageType: 'sender'),
+    ChatMessage(
+        messageContent:
+            'What would happen if I make a really long string??? I really wonder what\'s gonna happen',
+        messageType: 'receiver'),
+  ].obs;
 
   @override
   Widget build(BuildContext context) {
+    final _screenWidth = MediaQuery.of(context).size.width.obs;
     return Container(
       color: _themeController.theme.colorSet.backgroundColor,
       child: SafeArea(
@@ -17,52 +36,142 @@ class ChatroomPage extends StatelessWidget {
             appBar: AppBar(
               title: Text('CHAT'),
               backgroundColor: Colors.white,
-              iconTheme: IconThemeData(color: _themeController.theme.colorSet.sociableBlue),
-              textTheme: TextTheme(headline6: TextStyle(color: _themeController.theme.colorSet.sociableBlue)),
+              iconTheme: IconThemeData(
+                  color: _themeController.theme.colorSet.sociableBlue),
+              textTheme: TextTheme(
+                  headline6: TextStyle(
+                      color: _themeController.theme.colorSet.sociableBlue)),
             ),
             body: Column(children: [
               Expanded(
-                child: Text("HI"),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 10, right: 10),
-                    child: GestureDetector(
-                      onTap: (){},
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          color: _themeController.theme.colorSet.sociableBlue,
-                          borderRadius: BorderRadius.circular(30),
+                  child: Obx(() => ListView.builder(
+                        controller: _chatScrollController,
+                        shrinkWrap: true,
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          return Align(
+                            alignment: messages[index].messageType == 'receiver'
+                                ? Alignment.topRight
+                                : Alignment.topLeft,
+                            child: Obx(() => Container(
+                                  constraints: BoxConstraints(
+                                    maxWidth: _screenWidth.value * 0.8,
+                                  ),
+                                  margin: EdgeInsets.only(
+                                      left: 10,
+                                      right: 10,
+                                      bottom: 8),
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 12, vertical: 8),
+                                  decoration: BoxDecoration(
+                                    border: messages[index].messageType ==
+                                            'receiver'
+                                        ? Border()
+                                        : Border.all(
+                                            color: _themeController
+                                                .theme.colorSet.sociableBlue),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: messages[index].messageType ==
+                                            'receiver'
+                                        ? _themeController
+                                            .theme.colorSet.sociableBlue
+                                        : Colors.white,
+                                  ),
+                                  child: Text(messages[index].messageContent,
+                                      style: TextStyle(
+                                        color: messages[index].messageType ==
+                                                'receiver'
+                                            ? Colors.white
+                                            : _themeController
+                                                .theme.colorSet.sociableBlue,
+                                        fontSize: 18,
+                                      )),
+                                )),
+                          );
+                        },
+                      ))),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                  color: _themeController.theme.colorSet.sociableBlue,
+                ))),
+                child: Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: _themeController
+                                    .theme.colorSet.sociableBlue,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Icon(Icons.add, color: Colors.white),
+                            ))),
+                    Expanded(
+                        child: TextField(
+                      controller: _chatInputController,
+                      decoration: InputDecoration(
+                        hintText: 'Write message...',
+                        hintStyle: TextStyle(
+                            color: _themeController.theme.colorSet.hintText),
+                        border: InputBorder.none,
+                      ),
+                    )),
+                    Container(
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      child: TextButton(
+                        onPressed: () => _pressSend(context),
+                        child: Text(
+                          'SEND',
+                          style: TextStyle(color: Colors.white),
                         ),
-                        child: Icon(Icons.add, color: Colors.white),
-                      )
+                        style: _themeController.chatButtonStyle,
+                      ),
                     )
-                  ),
-                  Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Write message...',
-                          hintStyle: TextStyle(color: _themeController.theme.colorSet.hintText),
-                          border: InputBorder.none,
-                        ),
-                      )
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10, right: 10),
-                    child: TextButton(
-                        onPressed: (){},
-                      child: Text('SEND', style: TextStyle(color: Colors.white),),
-                      style: _themeController.chatButtonStyle,
-                    ),
-                  )
-                ],
+                  ],
+                ),
               )
             ])),
       ),
     );
-    throw UnimplementedError();
   }
+
+  void _pressSend(BuildContext context) {
+    final newMessage = ChatMessage(
+        messageContent: _chatInputController.text, messageType: 'receiver');
+    messages.add(newMessage);
+    _chatInputController.text = '';
+
+    final TextPainter _textPainter = TextPainter(
+      text: TextSpan(
+        text: newMessage.messageContent,
+        style: TextStyle(fontSize: 18),
+      ),
+      textDirection: TextDirection.ltr,
+    )..layout(
+        minWidth: 0, maxWidth: MediaQuery.of(context).size.width * 0.8 - 40);
+
+    final _newMessageHeight = _textPainter.size.height;
+
+    _chatScrollController.animateTo(
+      _chatScrollController.position.maxScrollExtent + _newMessageHeight + 30,
+      duration: Duration(milliseconds: 100),
+      curve: Curves.easeOut,
+    );
+  }
+}
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+
+  ChatMessage({
+    @required this.messageContent,
+    @required this.messageType,
+  });
 }
